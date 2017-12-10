9d1077acbc9f28b2abdc47a3d02bb53a826188e8
Snorre Lothar von Gohren Edwin <snorre.edwin@gmail.com>
2017-12-10 01:31:14 +0100

    landed the summarizing

--- src/services/balance.js
+++ src/services/balance.js
@@ -10,11 +10,24 @@ const getTotalBalance = async (functions) => {
     const bittrexBalance = await bittrex(bittrexConfig.key, bittrexConfig.secret);
     const etherBalance = await etherwallet();
 
-    const all = bittrexBalance.concat(bitfinexBalance).concat(etherBalance);
+    let all = bittrexBalance.concat(bitfinexBalance).concat(etherBalance);
+    all = all.filter(currency => {
+      if(currency.ticker  === 'BTC') return true;
+      else if(currency.balance > 0.1) return true;
+    })
+    const balance = _.chain(all).groupBy('ticker').mapValues(v => {
+      return v.map(returnBalance).reduce(sum);
+    })
 
-    const whaat = _.chain(all).groupBy('ticker').flattenDeep()
-
-    console.log(whaat.value())
+    return balance.value()
 };
+
+const returnBalance = (object) => {
+  return object.balance;
+}
+
+const sum = (sum, v) => {
+  return sum+v;
+}
 
 export default getTotalBalance;
