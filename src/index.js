0b77e30e84e6d89e8903ddf37d0da85346071d5d
Snorre Lothar von Gohren Edwin <snorre.edwin@gmail.com>
2017-12-22 09:44:19 +0100

    removed unused functions and added logger to ultimate function

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-const functions = require('firebase-functions');
+import * as functions from 'firebase-functions';
+import logger from './utils/logger';
 import express from 'express';
 import bitfinex from './lib/bitfinex';
 import bittrex from './lib/bittrex';
@@ -6,39 +7,11 @@ import etherwallet from './lib/etherwallet';
 import getBalance from './services/balance';
 import security from './middleware/security';
 
-process.on('unhandledRejection', r => console.log(r))
+process.on('unhandledRejection', r => logger.log('error',r))
 
 let app = express();
 
 app.use(security)
-
-exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
-});
-
-exports.getBitfinexBalance = functions.https.onRequest(async (request, response) => {
-  console.log(bitfinex)
-  const bitfinexConfig = functions.config().bitfinex
-  const balance = await bitfinex(bitfinexConfig.key, bitfinexConfig.secret);
-  response.send(balance);
-});
-
-exports.getBittrexBalance = functions.https.onRequest(async (request, response) => {
-  const bittrexConfig = functions.config().bittrex
-  const balance = await bittrex(bittrexConfig.key, bittrexConfig.secret);
-  response.send(balance);
-});
-
-exports.getEtherBalance = functions.https.onRequest(async (request, response) => {
-  const balance = await etherwallet();
-  response.send(balance);
-});
-
-exports.getBalance = functions.https.onRequest(async (request, response) => {
-  const balance = await getBalance(functions);
-  response.send(balance);
-});
-
 app.get('/getBalances', async (req, res) => {
   const balance = await getBalance(functions)
   res.send(balance)
